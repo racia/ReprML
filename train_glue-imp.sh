131e3d4a87b5bcec31b66048c5fe834a41c8e5ed
Raziye Sari <sari@cl.uni-heidelberg.de>
2026-06-06 21:47:58 +0200

    Correct FP rate handling and more scripts

---
+++ train_glue-imp.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#SBATCH --job-name=reprML
+#SBATCH --output=reprML-glue-imp.out
+#SBATCH --error=reprML-glue-imp.err
+# SBATCH --time=03:29:00 # Approx. 3 1/2 hrs
+# SBATCH --partition=gpu
+#SBATCH --gres=gpu:2
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G
+#SBATCH --mail-type=all
+#SBATCH --mail-user=sari@cl.uni-heidelberg.de
+
+# Save input variables
+if (! -z $1); then
+    NOISE="-imp"
+fi
+
+ENV="repr"
+
+# Activate the conda environment
+source "$(conda info --base)/etc/profile.d/conda.sh"
+conda activate $ENV
+
+# Monitor GPU usage in background
+(
+    while true; do
+        echo "== GPU Status: $(date) =="
+        nvidia-smi --query-gpu=index,utilization.memory,utilization.gpu --format=csv
+        sleep 30
+    done
+) > gpu_monitor.log &
+MONITOR_PID=$!#
+
+# Define script and config variables
+declare -a CONFIGS=("$PWD/config/train-glue"${NOISE}".yaml")
+SCRIPT="trainer.py"
+echo "Running $script with the following configurations: ${CONFIGS[*]}"
+
+python "$SCRIPT" --config "${CONFIGS[@]}"
